584cbbe3e94b651b44dc0f5dad74911d0c253317
mihil2701 <39548558+mihil2701@users.noreply.github.com>
2019-05-03 06:42:27 +0530

    Update predict.py

--- morph_generation/predict.py
+++ morph_generation/predict.py
@@ -1,1 +1,10 @@
-def (a):
+sample_output="sample_output.txt"
+def pred():
+  f=open(sample_output,"r")
+  l1=f.readline()
+  while(l1):
+    l=[]
+    l1=l1.rstrip('\n')
+    l.extend(l1.split(" "))
+    l1=f.readline()
+  return l
